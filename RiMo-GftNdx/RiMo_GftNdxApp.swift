06eb3128b2be77406a8d5957f66355b627a64cb1
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-22 09:12:38 +0100

    Initial Commit

---
+++ RiMo-GftNdx/RiMo_GftNdxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiMo_GftNdxApp.swift
+//  RiMo-GftNdx
+//
+//  Created by Javier Calatrava on 22/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RiMo_GftNdxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RiMo-GftNdxTests/RiMo_GftNdxTests.swift
@@ -0,0 +1,17 @@
+//
+//  RiMo_GftNdxTests.swift
+//  RiMo-GftNdxTests
+//
+//  Created by Javier Calatrava on 22/3/25.
+//
+
+import Testing
+@testable import RiMo_GftNdx
+
+struct RiMo_GftNdxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
